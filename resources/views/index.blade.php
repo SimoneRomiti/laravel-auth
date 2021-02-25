0042503cf7bf292aca55d37fcd41915ab90b34d4
Simone Romiti <sr1992ei@gmail.com>
2021-02-25 18:25:55 +0100

    Index per guest con link alla show via login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<h1>Tutti i post</h1>
+
+		<div class="d-flex">
+			@foreach ($posts as $post)
+				<div class="card" style="width: calc(100% / 4 - 30px); margin: 15px;">
+					<img src="{{ $post->image }}" class="card-img-top" alt="...">
+					<div class="card-body" style="display: flex; flex-direction:column; justify-content: space-between">
+						<h2>{{ $post->title }}</h2>
+						<p>{{ substr($post->text, 0, 200). '...' }}</p>
+						<p>- <strong>{{ $post->user->name }}</strong></p>
+						<a href="{{ route('admin.posts.show', $post->slug) }}" class="btn btn-primary">Dettaglio</a>
+					</div>
+				</div>
+			@endforeach
+		</div>
+
+	</div>
+		
+@endsection
